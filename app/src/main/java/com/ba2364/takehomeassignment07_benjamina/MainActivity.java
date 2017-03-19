30aca4e94f46def1be0b0903697a96a436d17435
benjaminantell <ba2364@tc.columbia.edu>
2017-03-19 16:36:11 -0400

    Initial commit

---
+++ app/src/main/java/com/ba2364/takehomeassignment07_benjamina/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ba2364.takehomeassignment07_benjamina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
